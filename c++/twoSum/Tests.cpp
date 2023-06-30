c1bf9e24c88e65102b7681d36f69f9be2b5a998f
George Risi <george.risi.jr@gmail.com>
2023-06-29 21:27:32 -0400

    Made the google test infrastructure build/link properly... took 2 hours

--- c++/twoSum/Tests.cpp
+++ c++/twoSum/Tests.cpp
@@ -4,12 +4,35 @@
 	-109 <= target <= 109
  */
 
+#include <vector>
 #include "gtest/gtest.h"
 #include "Solution.h"
 
-int main(int argc, char* argv[]){
+class TwoSumTest : public ::testing::Test{
+public:
+	TwoSumTest(){
+		// Need any setup here?
+	}
+
+	~TwoSumTest() override{
+		// Anything here?
+	}
+};
+
+TEST(TwoSumTest, nominal){
+	{
+		std::vector<int> nums{2,7,11,15};
+		Solution sol;
+		std::vector<int> ans = sol.twoSum(nums,9);
+		std::vector<int> expected{1,2};
+
+		EXPECT_EQ(expected, ans);
+	}
+
+}
+
+int main(int argc, char** argv){
    
-
-
-	Solution sol()
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
