1350598b0b8b81d642498ea27694f947f2270124
George Risi <george.risi.jr@gmail.com>
2023-07-04 21:14:41 -0400

    Backbone for productArrayExceptSelf

---
+++ c++/productArrayExceptSelf/Tests.cpp
@@ -0,0 +1,22 @@
+/*
+2 <= nums.length <= 105
+-30 <= nums[i] <= 30
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+ */
+
+#include "gtest/gtest.h"
+
+class ProductExceptSelfTest : public ::testing::Test{
+
+	ProductExceptSelfTest(){
+		// Setup
+	}
+
+	~ProductExceptSelfTest(){
+		// Teardown
+	}
+};
+
+TEST(ProductExceptSelfTest, nominal){
+
+}
